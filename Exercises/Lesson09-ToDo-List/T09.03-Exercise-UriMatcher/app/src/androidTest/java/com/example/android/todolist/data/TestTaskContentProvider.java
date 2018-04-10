8596d183f2eaabd5691feeb54b5ada22c20a6118
An Thai <steventhaiusa@yahoo.com>
2018-04-10 04:14:42 -0400

    Lesson09-ToDo-List: T09.03-Exercise-UriMatcher

--- Exercises/Lesson09-ToDo-List/T09.03-Exercise-UriMatcher/app/src/androidTest/java/com/example/android/todolist/data/TestTaskContentProvider.java
+++ Exercises/Lesson09-ToDo-List/T09.03-Exercise-UriMatcher/app/src/androidTest/java/com/example/android/todolist/data/TestTaskContentProvider.java
@@ -17,10 +17,16 @@
 package com.example.android.todolist.data;
 
 import android.content.ComponentName;
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.ContentValues;
 import android.content.Context;
+import android.content.UriMatcher;
 import android.content.pm.PackageManager;
 import android.content.pm.ProviderInfo;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -34,6 +40,7 @@ import org.junit.runner.RunWith;
 
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.fail;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(AndroidJUnit4.class)
 public class TestTaskContentProvider {
@@ -123,39 +130,39 @@ public class TestTaskContentProvider {
     //================================================================================
 
 
-//    private static final Uri TEST_TASKS = TaskContract.TaskEntry.CONTENT_URI;
-//    // Content URI for a single task with id = 1
-//    private static final Uri TEST_TASK_WITH_ID = TEST_TASKS.buildUpon().appendPath("1").build();
-//
-//
-//    /**
-//     * This function tests that the UriMatcher returns the correct integer value for
-//     * each of the Uri types that the ContentProvider can handle. Uncomment this when you are
-//     * ready to test your UriMatcher.
-//     */
-//    @Test
-//    public void testUriMatcher() {
-//
-//        /* Create a URI matcher that the TaskContentProvider uses */
-//        UriMatcher testMatcher = TaskContentProvider.buildUriMatcher();
-//
-//        /* Test that the code returned from our matcher matches the expected TASKS int */
-//        String tasksUriDoesNotMatch = "Error: The TASKS URI was matched incorrectly.";
-//        int actualTasksMatchCode = testMatcher.match(TEST_TASKS);
-//        int expectedTasksMatchCode = TaskContentProvider.TASKS;
-//        assertEquals(tasksUriDoesNotMatch,
-//                actualTasksMatchCode,
-//                expectedTasksMatchCode);
-//
-//        /* Test that the code returned from our matcher matches the expected TASK_WITH_ID */
-//        String taskWithIdDoesNotMatch =
-//                "Error: The TASK_WITH_ID URI was matched incorrectly.";
-//        int actualTaskWithIdCode = testMatcher.match(TEST_TASK_WITH_ID);
-//        int expectedTaskWithIdCode = TaskContentProvider.TASK_WITH_ID;
-//        assertEquals(taskWithIdDoesNotMatch,
-//                actualTaskWithIdCode,
-//                expectedTaskWithIdCode);
-//    }
+    private static final Uri TEST_TASKS = TaskContract.TaskEntry.CONTENT_URI;
+    // Content URI for a single task with id = 1
+    private static final Uri TEST_TASK_WITH_ID = TEST_TASKS.buildUpon().appendPath("1").build();
+
+
+    /**
+     * This function tests that the UriMatcher returns the correct integer value for
+     * each of the Uri types that the ContentProvider can handle. Uncomment this when you are
+     * ready to test your UriMatcher.
+     */
+    @Test
+    public void testUriMatcher() {
+
+        /* Create a URI matcher that the TaskContentProvider uses */
+        UriMatcher testMatcher = TaskContentProvider.buildUriMatcher();
+
+        /* Test that the code returned from our matcher matches the expected TASKS int */
+        String tasksUriDoesNotMatch = "Error: The TASKS URI was matched incorrectly.";
+        int actualTasksMatchCode = testMatcher.match(TEST_TASKS);
+        int expectedTasksMatchCode = TaskContentProvider.TASKS;
+        assertEquals(tasksUriDoesNotMatch,
+                actualTasksMatchCode,
+                expectedTasksMatchCode);
+
+        /* Test that the code returned from our matcher matches the expected TASK_WITH_ID */
+        String taskWithIdDoesNotMatch =
+                "Error: The TASK_WITH_ID URI was matched incorrectly.";
+        int actualTaskWithIdCode = testMatcher.match(TEST_TASK_WITH_ID);
+        int expectedTaskWithIdCode = TaskContentProvider.TASK_WITH_ID;
+        assertEquals(taskWithIdDoesNotMatch,
+                actualTaskWithIdCode,
+                expectedTaskWithIdCode);
+    }
 
 
     //================================================================================
@@ -163,52 +170,52 @@ public class TestTaskContentProvider {
     //================================================================================
 
 
-//    /**
-//     * Tests inserting a single row of data via a ContentResolver
-//     */
-//    @Test
-//    public void testInsert() {
-//
-//        /* Create values to insert */
-//        ContentValues testTaskValues = new ContentValues();
-//        testTaskValues.put(TaskContract.TaskEntry.COLUMN_DESCRIPTION, "Test description");
-//        testTaskValues.put(TaskContract.TaskEntry.COLUMN_PRIORITY, 1);
-//
-//        /* TestContentObserver allows us to test if notifyChange was called appropriately */
-//        TestUtilities.TestContentObserver taskObserver = TestUtilities.getTestContentObserver();
-//
-//        ContentResolver contentResolver = mContext.getContentResolver();
-//
-//        /* Register a content observer to be notified of changes to data at a given URI (tasks) */
-//        contentResolver.registerContentObserver(
-//                /* URI that we would like to observe changes to */
-//                TaskContract.TaskEntry.CONTENT_URI,
-//                /* Whether or not to notify us if descendants of this URI change */
-//                true,
-//                /* The observer to register (that will receive notifyChange callbacks) */
-//                taskObserver);
-//
-//
-//        Uri uri = contentResolver.insert(TaskContract.TaskEntry.CONTENT_URI, testTaskValues);
-//
-//
-//        Uri expectedUri = ContentUris.withAppendedId(TaskContract.TaskEntry.CONTENT_URI, 1);
-//
-//        String insertProviderFailed = "Unable to insert item through Provider";
-//        assertEquals(insertProviderFailed, uri, expectedUri);
-//
-//        /*
-//         * If this fails, it's likely you didn't call notifyChange in your insert method from
-//         * your ContentProvider.
-//         */
-//        taskObserver.waitForNotificationOrFail();
-//
-//        /*
-//         * waitForNotificationOrFail is synchronous, so after that call, we are done observing
-//         * changes to content and should therefore unregister this observer.
-//         */
-//        contentResolver.unregisterContentObserver(taskObserver);
-//    }
+    /**
+     * Tests inserting a single row of data via a ContentResolver
+     */
+    @Test
+    public void testInsert() {
+
+        /* Create values to insert */
+        ContentValues testTaskValues = new ContentValues();
+        testTaskValues.put(TaskContract.TaskEntry.COLUMN_DESCRIPTION, "Test description");
+        testTaskValues.put(TaskContract.TaskEntry.COLUMN_PRIORITY, 1);
+
+        /* TestContentObserver allows us to test if notifyChange was called appropriately */
+        TestUtilities.TestContentObserver taskObserver = TestUtilities.getTestContentObserver();
+
+        ContentResolver contentResolver = mContext.getContentResolver();
+
+        /* Register a content observer to be notified of changes to data at a given URI (tasks) */
+        contentResolver.registerContentObserver(
+                /* URI that we would like to observe changes to */
+                TaskContract.TaskEntry.CONTENT_URI,
+                /* Whether or not to notify us if descendants of this URI change */
+                true,
+                /* The observer to register (that will receive notifyChange callbacks) */
+                taskObserver);
+
+
+        Uri uri = contentResolver.insert(TaskContract.TaskEntry.CONTENT_URI, testTaskValues);
+
+
+        Uri expectedUri = ContentUris.withAppendedId(TaskContract.TaskEntry.CONTENT_URI, 1);
+
+        String insertProviderFailed = "Unable to insert item through Provider";
+        assertEquals(insertProviderFailed, uri, expectedUri);
+
+        /*
+         * If this fails, it's likely you didn't call notifyChange in your insert method from
+         * your ContentProvider.
+         */
+        taskObserver.waitForNotificationOrFail();
+
+        /*
+         * waitForNotificationOrFail is synchronous, so after that call, we are done observing
+         * changes to content and should therefore unregister this observer.
+         */
+        contentResolver.unregisterContentObserver(taskObserver);
+    }
 
 
     //================================================================================
@@ -216,54 +223,54 @@ public class TestTaskContentProvider {
     //================================================================================
 
 
-//    /**
-//     * Inserts data, then tests if a query for the tasks directory returns that data as a Cursor
-//     */
-//    @Test
-//    public void testQuery() {
-//
-//        /* Get access to a writable database */
-//        TaskDbHelper dbHelper = new TaskDbHelper(mContext);
-//        SQLiteDatabase database = dbHelper.getWritableDatabase();
-//
-//        /* Create values to insert */
-//        ContentValues testTaskValues = new ContentValues();
-//        testTaskValues.put(TaskContract.TaskEntry.COLUMN_DESCRIPTION, "Test description");
-//        testTaskValues.put(TaskContract.TaskEntry.COLUMN_PRIORITY, 1);
-//
-//        /* Insert ContentValues into database and get a row ID back */
-//        long taskRowId = database.insert(
-//                /* Table to insert values into */
-//                TaskContract.TaskEntry.TABLE_NAME,
-//                null,
-//                /* Values to insert into table */
-//                testTaskValues);
-//
-//        String insertFailed = "Unable to insert directly into the database";
-//        assertTrue(insertFailed, taskRowId != -1);
-//
-//        /* We are done with the database, close it now. */
-//        database.close();
-//
-//        /* Perform the ContentProvider query */
-//        Cursor taskCursor = mContext.getContentResolver().query(
-//                TaskContract.TaskEntry.CONTENT_URI,
-//                /* Columns; leaving this null returns every column in the table */
-//                null,
-//                /* Optional specification for columns in the "where" clause above */
-//                null,
-//                /* Values for "where" clause */
-//                null,
-//                /* Sort order to return in Cursor */
-//                null);
-//
-//
-//        String queryFailed = "Query failed to return a valid Cursor";
-//        assertTrue(queryFailed, taskCursor != null);
-//
-//        /* We are done with the cursor, close it now. */
-//        taskCursor.close();
-//    }
+    /**
+     * Inserts data, then tests if a query for the tasks directory returns that data as a Cursor
+     */
+    @Test
+    public void testQuery() {
+
+        /* Get access to a writable database */
+        TaskDbHelper dbHelper = new TaskDbHelper(mContext);
+        SQLiteDatabase database = dbHelper.getWritableDatabase();
+
+        /* Create values to insert */
+        ContentValues testTaskValues = new ContentValues();
+        testTaskValues.put(TaskContract.TaskEntry.COLUMN_DESCRIPTION, "Test description");
+        testTaskValues.put(TaskContract.TaskEntry.COLUMN_PRIORITY, 1);
+
+        /* Insert ContentValues into database and get a row ID back */
+        long taskRowId = database.insert(
+                /* Table to insert values into */
+                TaskContract.TaskEntry.TABLE_NAME,
+                null,
+                /* Values to insert into table */
+                testTaskValues);
+
+        String insertFailed = "Unable to insert directly into the database";
+        assertTrue(insertFailed, taskRowId != -1);
+
+        /* We are done with the database, close it now. */
+        database.close();
+
+        /* Perform the ContentProvider query */
+        Cursor taskCursor = mContext.getContentResolver().query(
+                TaskContract.TaskEntry.CONTENT_URI,
+                /* Columns; leaving this null returns every column in the table */
+                null,
+                /* Optional specification for columns in the "where" clause above */
+                null,
+                /* Values for "where" clause */
+                null,
+                /* Sort order to return in Cursor */
+                null);
+
+
+        String queryFailed = "Query failed to return a valid Cursor";
+        assertTrue(queryFailed, taskCursor != null);
+
+        /* We are done with the cursor, close it now. */
+        taskCursor.close();
+    }
 
 
     //================================================================================
@@ -271,69 +278,69 @@ public class TestTaskContentProvider {
     //================================================================================
 
 
-//    /**
-//     * Tests deleting a single row of data via a ContentResolver
-//     */
-//    @Test
-//    public void testDelete() {
-//        /* Access writable database */
-//        TaskDbHelper helper = new TaskDbHelper(InstrumentationRegistry.getTargetContext());
-//        SQLiteDatabase database = helper.getWritableDatabase();
-//
-//        /* Create a new row of task data */
-//        ContentValues testTaskValues = new ContentValues();
-//        testTaskValues.put(TaskContract.TaskEntry.COLUMN_DESCRIPTION, "Test description");
-//        testTaskValues.put(TaskContract.TaskEntry.COLUMN_PRIORITY, 1);
-//
-//        /* Insert ContentValues into database and get a row ID back */
-//        long taskRowId = database.insert(
-//                /* Table to insert values into */
-//                TaskContract.TaskEntry.TABLE_NAME,
-//                null,
-//                /* Values to insert into table */
-//                testTaskValues);
-//
-//        /* Always close the database when you're through with it */
-//        database.close();
-//
-//        String insertFailed = "Unable to insert into the database";
-//        assertTrue(insertFailed, taskRowId != -1);
-//
-//
-//        /* TestContentObserver allows us to test if notifyChange was called appropriately */
-//        TestUtilities.TestContentObserver taskObserver = TestUtilities.getTestContentObserver();
-//
-//        ContentResolver contentResolver = mContext.getContentResolver();
-//
-//        /* Register a content observer to be notified of changes to data at a given URI (tasks) */
-//        contentResolver.registerContentObserver(
-//                /* URI that we would like to observe changes to */
-//                TaskContract.TaskEntry.CONTENT_URI,
-//                /* Whether or not to notify us if descendants of this URI change */
-//                true,
-//                /* The observer to register (that will receive notifyChange callbacks) */
-//                taskObserver);
-//
-//
-//
-//        /* The delete method deletes the previously inserted row with id = 1 */
-//        Uri uriToDelete = TaskContract.TaskEntry.CONTENT_URI.buildUpon().appendPath("1").build();
-//        int tasksDeleted = contentResolver.delete(uriToDelete, null, null);
-//
-//        String deleteFailed = "Unable to delete item in the database";
-//        assertTrue(deleteFailed, tasksDeleted != 0);
-//
-//        /*
-//         * If this fails, it's likely you didn't call notifyChange in your delete method from
-//         * your ContentProvider.
-//         */
-//        taskObserver.waitForNotificationOrFail();
-//
-//        /*
-//         * waitForNotificationOrFail is synchronous, so after that call, we are done observing
-//         * changes to content and should therefore unregister this observer.
-//         */
-//        contentResolver.unregisterContentObserver(taskObserver);
-//    }
+    /**
+     * Tests deleting a single row of data via a ContentResolver
+     */
+    @Test
+    public void testDelete() {
+        /* Access writable database */
+        TaskDbHelper helper = new TaskDbHelper(InstrumentationRegistry.getTargetContext());
+        SQLiteDatabase database = helper.getWritableDatabase();
+
+        /* Create a new row of task data */
+        ContentValues testTaskValues = new ContentValues();
+        testTaskValues.put(TaskContract.TaskEntry.COLUMN_DESCRIPTION, "Test description");
+        testTaskValues.put(TaskContract.TaskEntry.COLUMN_PRIORITY, 1);
+
+        /* Insert ContentValues into database and get a row ID back */
+        long taskRowId = database.insert(
+                /* Table to insert values into */
+                TaskContract.TaskEntry.TABLE_NAME,
+                null,
+                /* Values to insert into table */
+                testTaskValues);
+
+        /* Always close the database when you're through with it */
+        database.close();
+
+        String insertFailed = "Unable to insert into the database";
+        assertTrue(insertFailed, taskRowId != -1);
+
+
+        /* TestContentObserver allows us to test if notifyChange was called appropriately */
+        TestUtilities.TestContentObserver taskObserver = TestUtilities.getTestContentObserver();
+
+        ContentResolver contentResolver = mContext.getContentResolver();
+
+        /* Register a content observer to be notified of changes to data at a given URI (tasks) */
+        contentResolver.registerContentObserver(
+                /* URI that we would like to observe changes to */
+                TaskContract.TaskEntry.CONTENT_URI,
+                /* Whether or not to notify us if descendants of this URI change */
+                true,
+                /* The observer to register (that will receive notifyChange callbacks) */
+                taskObserver);
+
+
+
+        /* The delete method deletes the previously inserted row with id = 1 */
+        Uri uriToDelete = TaskContract.TaskEntry.CONTENT_URI.buildUpon().appendPath("1").build();
+        int tasksDeleted = contentResolver.delete(uriToDelete, null, null);
+
+        String deleteFailed = "Unable to delete item in the database";
+        assertTrue(deleteFailed, tasksDeleted != 0);
+
+        /*
+         * If this fails, it's likely you didn't call notifyChange in your delete method from
+         * your ContentProvider.
+         */
+        taskObserver.waitForNotificationOrFail();
+
+        /*
+         * waitForNotificationOrFail is synchronous, so after that call, we are done observing
+         * changes to content and should therefore unregister this observer.
+         */
+        contentResolver.unregisterContentObserver(taskObserver);
+    }
 
 }
